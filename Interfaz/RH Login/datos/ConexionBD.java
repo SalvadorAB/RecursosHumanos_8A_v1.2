818daeb4c8dceae38a9635aad2aa2b6fd05db428
SalvadorAB <72758367+SalvadorAB@users.noreply.github.com>
2021-03-08 00:26:32 -0600

    Add files via upload

---
+++ Interfaz/RH Login/datos/ConexionBD.java
@@ -0,0 +1,59 @@
+package datos;
+
+/**
+ *
+ * @author salva
+ */
+    
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class ConexionBD {
+    
+    Connection con;
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String url = "jdbc:mysql://localhost:3306/RecursosHumanos";
+    
+    public Connection conexion(String user, String pass){
+        
+        con = null;
+        
+        try{
+            Class.forName(driver);
+            con = (Connection) DriverManager.getConnection(url, user, pass);
+            if(con != null){
+                JOptionPane.showMessageDialog(null, "Conexión establecida");
+            }
+            
+        } catch (ClassNotFoundException |  SQLException e){
+            JOptionPane.showMessageDialog(null, "Error de conexion: "+e.getMessage());
+        }
+        return con;
+        
+    }
+    
+    public boolean probarConexion(String user, String pass){
+        con = null;
+        boolean b = false;
+        
+        try{
+            Class.forName(driver);
+            con = (Connection) DriverManager.getConnection(url, user, pass);
+            if(con != null){
+                b = true;
+                JOptionPane.showMessageDialog(null, "Conexión establecida");
+            }
+            
+        } catch (ClassNotFoundException |  SQLException e){
+            b = false;
+            JOptionPane.showMessageDialog(null, "Error de conexion: "+e.getMessage());
+        }
+        return b;
+    }
+            
+    
+}
---
+++ Interfaz/RH Login/datos/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+import Vistas.Deducciones;
+import java.sql.Connection;
+import login.Login;
+
+/**
+ *
+ * @author salva
+ */
+public class Main {
+    
+    public static void main(String args[]) {
+       
+        
+        Deducciones d = new Deducciones();
+        d.setVisible(true);
+    }
+    
+}
